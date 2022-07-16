6dc17f0d58f23711e03b485a0b59093f2a116817
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-16 16:03:44 +0530

    Leap Year Program

--- Day5PracticeProblem/Program.cs
+++ Day5PracticeProblem/Program.cs
@@ -1,21 +1,10 @@
-﻿int noOftimes = 5;
-int heads=0, tails=0;
-double hPercentage, tPercentage;
-Random random = new Random();
-for(int i = 0; i < noOftimes; i++)
+﻿Console.WriteLine("Enter the year: ");
+int year=int.Parse(Console.ReadLine());
+if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
 {
-    int coinFlip = random.Next(0, 2);
-    if (coinFlip < 0.5)
-    {
-        Console.WriteLine("Its Tails");
-        tails++;
-    }
-    else
-    {
-        Console.WriteLine("Its Heads");
-        heads++;
-    }
+    Console.WriteLine(year+" is a Leap Year");
 }
-hPercentage = 100 * heads / noOftimes;
-tPercentage = 100 * tails / noOftimes;
-Console.WriteLine("Heads % : " +hPercentage+ " vs Tails % : " +tPercentage);
+else
+{
+    Console.WriteLine(year+" is not a Leap Year");
+}
