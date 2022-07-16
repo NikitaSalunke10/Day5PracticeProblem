a845f013bb31aefed83e9801d876cb041cb49dd7
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-16 21:09:46 +0530

    Swapping 2 numbers

--- Day5PracticeProblem/Program.cs
+++ Day5PracticeProblem/Program.cs
@@ -1,10 +1,8 @@
-﻿Console.WriteLine("Enter the Dividend: ");
-int dividend = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter the Divisor");
-int divisor = int.Parse(Console.ReadLine());
-int remainder;
-float quotient;
-quotient = dividend / divisor;
-remainder = dividend % divisor;
-Console.WriteLine("Remainder: "+remainder);
-Console.WriteLine("Quotient: "+quotient);
+﻿int num1 = 15, num2 = 30, temp;
+Console.WriteLine("Before Swapping:");
+Console.WriteLine("Num 1: " +num1+ " Num 2: " +num2);
+temp = num1;
+num1 = num2;
+num2 = temp;
+Console.WriteLine("After Swapping:");
+Console.WriteLine("Num 1: " + num1 + " Num 2: " + num2);
