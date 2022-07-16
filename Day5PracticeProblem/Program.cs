06cc47f24e151e03655699a90ccad94c8d3eed21
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-16 16:35:56 +0530

    Harmonic number problem

--- Day5PracticeProblem/Program.cs
+++ Day5PracticeProblem/Program.cs
@@ -1,6 +1,8 @@
-﻿String num = args[0];
-int n=int.Parse(num);
-for(int i=1; i<=n; i++)
+﻿Console.WriteLine("Enter the value of N: ");
+int n=int.Parse(Console.ReadLine());
+double harmonicNo=0.0;
+for (double i=1; i<=n; i++)
 {
-    Console.WriteLine(Math.Pow(2,i));
+    harmonicNo = harmonicNo + (1/i);
 }
+Console.WriteLine("Harmonic value of "+n+" is : "+harmonicNo);
