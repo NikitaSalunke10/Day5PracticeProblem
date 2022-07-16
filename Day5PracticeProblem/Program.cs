954b6f11dc6706fa2415f8f91617c0f76d7b1fce
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-16 15:53:35 +0530

    Add project files.

---
+++ Day5PracticeProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿int noOftimes = 5;
+int heads=0, tails=0;
+double hPercentage, tPercentage;
+Random random = new Random();
+for(int i = 0; i < noOftimes; i++)
+{
+    int coinFlip = random.Next(0, 2);
+    if (coinFlip < 0.5)
+    {
+        Console.WriteLine("Its Tails");
+        tails++;
+    }
+    else
+    {
+        Console.WriteLine("Its Heads");
+        heads++;
+    }
+}
+hPercentage = 100 * heads / noOftimes;
+tPercentage = 100 * tails / noOftimes;
+Console.WriteLine("Heads % : " +hPercentage+ " vs Tails % : " +tPercentage);
